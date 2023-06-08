d4355c7beb0258f004ea599fbac627b3c5c529df
Asraful Hoque <96014879+asrafulhoque25@users.noreply.github.com>
2023-06-08 15:24:56 +0600

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,46 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./src/**/*.{html,js}",
+    "{html,js}", './index.html'
+  ], 
+  theme: {
+    fontFamily: {
+      'poppins': ['Poppins', 'sans-serif'],
+      'body': ['"Roboto"'],
+    },
+
+
+
+    screens: {
+      'sm': '640px',
+      'md': '768px',
+      'lg': '992px',
+      'xl': '1110px',
+    },
+
+
+    extend: {
+
+      colors: {
+        //Primary colors
+        blue: "#0078D2",
+        main_dark: "#0B0140",
+        main_light: "#F5F3FD",
+
+        //Secondary Color
+        secondary: "#4AF8E3",
+        secondary_shade: "#FFF3EE",
+        secondary_light: "#F3EADA",
+
+        //Dark Color
+        dark1: "#06021C",
+        dark2: "#33313E",
+
+
+      },
+
+    },
+  },
+  plugins: [],
+}
